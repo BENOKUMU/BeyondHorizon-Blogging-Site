4ad0c14de38c27117ccb3d53906151d6fb833460
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-10-19 19:59:49 +0300

    Updated

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import InputBox from '../components/input.component'
 
 const UserAuthForm = ({ type }) => {
   return (
     <section className="h-cover flex items-center justify-center">
         <form action="" className="w-[80%] max-w-[400px]">
             <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-                { type = "sign-in" ? "Welcome back" : "Join us Today" }
+                { type == "sign-in" ? "Welcome back" : "Join us Today" }
             </h1>
+
+            {
+                type != "sign-in" ?
+                <InputBox 
+                    name="fullname"
+                    type="text"
+                    placeholder="Full Name"
+                    icon="fi-rs-user"
+                />
+                : ""
+            }
         </form>
     </section>
   )
