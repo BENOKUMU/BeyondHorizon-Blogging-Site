24f49b3cbbcd40c85fef918921941381d8e24cf7
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-10-20 15:23:52 +0300

    Update

---
+++ client/src/common/page-animation.jsx
@@ -0,0 +1,14 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
+const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 } }) => {
+  return (
+    <motion.div
+        initial={initial}
+        animmate={animate}
+    >
+        { children }
+    </motion.div>
+  )
+}
+
+export default AnimationWrapper
--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -1,50 +1,81 @@
 import React from 'react'
 import InputBox from '../components/input.component'
+import googleIcon from '../imgs/google.png'
+import { Link } from 'react-router-dom'
+import AnimationWrapper from '../common/page-animation'
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className="h-cover flex items-center justify-center">
-        <form action="" className="w-[80%] max-w-[400px]">
-            <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-                { type == "sign-in" ? "Welcome back" : "Join us Today" }
-            </h1>
+    <AnimationWrapper>
+        <section className="h-cover flex items-center justify-center">
+            <form action="" className="w-[80%] max-w-[400px]">
+                <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+                    { type == "sign-in" ? "Welcome back" : "Join us Today" }
+                </h1>
 
-            {
-                type != "sign-in" ?
+                {
+                    type != "sign-in" ?
+                    <InputBox 
+                        name="fullname"
+                        type="text"
+                        placeholder="Full Name"
+                        icon="fi-rs-user"
+                    />
+                    : ""
+                }
                 <InputBox 
-                    name="fullname"
-                    type="text"
-                    placeholder="Full Name"
-                    icon="fi-rs-user"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    icon="fi-rr-envelopes"
                 />
-                : ""
-            }
-            <InputBox 
-                name="email"
-                type="email"
-                placeholder="Email"
-                icon="fi-rr-envelopes"
-            />
-            <InputBox 
-                name="password"
-                type="password"
-                placeholder="Password"
-                icon="fi-rr-key"
-            />
+                <InputBox 
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    icon="fi-rr-key"
+                />
 
-            <button
-                className='btn-dark centre mt-14'
-                type='submit'
-            >
-                { type.replace("-", " ") }
-            </button>
+                <button
+                    className='btn-dark centre mt-14'
+                    type='submit'
+                >
+                    { type.replace("-", " ") }
+                </button>
 
-            <div className='relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold'>
-                <hr className='w-1/2 border-black' />
-                <hr className='w-1/2 border-black' />
-            </div>
-        </form>
-    </section>
+                <div className='relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold'>
+                    <hr className='w-1/2 border-black' />
+                    <p>or</p>
+                    <hr className='w-1/2 border-black' />
+                </div>
+
+                <button className='btn-dark flex items-center justify-center gap-4 w-[90%] center'>
+                    <img src={googleIcon} alt="" className='w-5' />
+                    Continue with Google
+                </button>
+
+                {
+
+                    type == "sign-in" ?
+                    <p className='mt-6 text-dark-grey text-xl text-center'>
+                        Don't have an account ?
+                        <Link to="/signup" className='underline text-black text-xl ml-1'>
+                            Join us today
+                        </Link>
+                    </p>
+                    :
+                    <p className='mt-6 text-dark-grey text-xl text-center'>
+                        Already a member ?
+                        <Link to="/signin" className='underline text-black text-xl ml-1'>
+                            Sign in here
+                        </Link>
+                    </p>              
+
+                }
+
+            </form>
+        </section>
+    </AnimationWrapper>
   )
 }
 
