193b614b5954dd0787fcc892f905b8c749880b83
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-10-20 16:12:02 +0300

    update

--- client/src/common/page-animation.jsx
+++ client/src/common/page-animation.jsx
@@ -12,3 +12,21 @@ const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opac
 }
 
 export default AnimationWrapper
+
+
+
+
+// import { AnimatePresence, motion } from 'framer-motion';
+
+// const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 } }) => {
+//   return (
+//     <motion.div
+//         initial={initial}
+//         animmate={animate}
+//     >
+//         { children }
+//     </motion.div>
+//   )
+// }
+
+// export default AnimationWrapper
